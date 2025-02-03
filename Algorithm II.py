6f8123dd97a70c247f2c6c5d35513aac2132038b
Anjisweety2 <145087377+Anjisweety2@users.noreply.github.com>
2025-02-03 10:37:48 -0600

    Update Algorithm II.py

--- Algorithm II.py
+++ Algorithm II.py
@@ -17,29 +17,17 @@ Return value: the n-th dodecahedral number
 def make_dodecahedral(n):
     return int(9/2*n**3-9/2*n**2+n)
 
+
 """
 Input values
-    icos_list: a list of numbers that are sums of icosahedral numbers
-    dodes_list: a list of numbers that are sums of dodecahedral numbers
+    m : maximum value of the positive integers that we are checking
     threshold: minimal value of the positive integers that we are checking
-    m: maximum value of the positive integers that we are checking
-
-Return value: Return the first integer that is between "threshold" and "m" such that
-              it cannot be written as a sum of two numbers from "icos_list" and "dodes_list"
-              respectively. If no such integer exists, then return None and print "no
-              counterexample".
+    lst: complete list of all possible sums after five rounds
+Return value: True if all numbers in the given range are in "lst"
+              False if there exists any counterexample.
 """
-def check_counterexample(icos_list, dodes_list, threshold, m):
-    result = [x + y for x in icos_list for y in dodes_list]
-    unique_result = list(set(result))  
-    counter = False
-    for i in list(range(threshold, m)):
-        if i not in unique_result:
-            print(i)
-            counter = True
-            break
-    if (not counter):
-        print("no counterexamples")
+def check_counter(threshold, m, lst):
+    return len(lst) == m - threshold + 1
 
 # Ex: (4,7)
 # It suffices to check 1.666*10^6 can be written as sums of at most 4 icosahedral numbers
@@ -68,6 +56,7 @@ for j in all_dodecahedrals:
         if ((i+j) <= int(1.666*10**6)):
             dode_second_step_combinations.add(i+j)
 
+
 #third step
 ico_third_step_combinations = set()
 for j in ico_second_step_combinations:
@@ -83,5 +72,11 @@ for j in ico_third_step_combinations:
             ico_fourth_step_combinations.add(i+j)
 
 
-check_counterexample(ico_fourth_step_combinations, dode_second_step_combinations, 10000, int(1.666*10**6)) 
+#last step
+final_list = set()
+for j in ico_fourth_step_combinations:
+    for i in dode_second_step_combinations:
+        if (10000 <= (i+j) <= int(1.666*10**6)):
+            final_list.add(i+j)
 
+check_counter(10000, int(1.666*10**6), final_list)
